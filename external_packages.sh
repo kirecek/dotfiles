4bcecc6fcff4013ed5fa2f99017a3197b13d58d6
kirecek <erikjankovic@gmail.com>
2016-08-08 22:20:11 +0200

    Add sh for collecting external packages

---
+++ external_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# plug.vim for neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
